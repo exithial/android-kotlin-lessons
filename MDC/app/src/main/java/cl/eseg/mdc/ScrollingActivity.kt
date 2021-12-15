671f38cdebc888724cc8d00584ce7d25479c908c
Enrique Solís Gálvez <esolis.externo@coopeuch.cl>
2021-12-15 08:19:31 -0300

    MDC - Preparing code

--- MDC/app/src/main/java/cl/eseg/mdc/ScrollingActivity.kt
+++ MDC/app/src/main/java/cl/eseg/mdc/ScrollingActivity.kt
@@ -19,12 +19,6 @@ private lateinit var binding: ActivityScrollingBinding
      binding = ActivityScrollingBinding.inflate(layoutInflater)
      setContentView(binding.root)
 
-        setSupportActionBar(findViewById(R.id.toolbar))
-        binding.toolbarLayout.title = title
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
